5687890e53e413f47540b4a2fe6298a65e55c7fc
Kaiser Nabola <kaiser.nabola@gmail.com>
2022-07-16 19:18:32 +0100

    second commit

--- first_flask/app.py
+++ first_flask/app.py
@@ -1,11 +1,15 @@
+import random
+
+import flask
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    num = random.randint(50, 100)
+    return flask.render_template('home/index.html', number=num)
 
 
 if __name__ == '__main__':
